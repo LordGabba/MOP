dee05b052427e148d481344e9b92bbc75342b81f
Gabba <gabriell123bruno@gmail.com>
2026-05-18 21:00:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const supabaseUrl = # Connect to Supabase via connection pooling
+DATABASE_URL="postgresql://postgres.pjeehaziodnxuakhacmc:[YOUR-PASSWORD]@aws-1-sa-east-1.pooler.supabase.com:6543/postgres?pgbouncer=true"
+
+# Direct connection to the database. Used for migrations
+DIRECT_URL="postgresql://postgres.pjeehaziodnxuakhacmc:[YOUR-PASSWORD]@aws-1-sa-east-1.pooler.supabase.com:5432/postgres"
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqZWVoYXppb2RueHVha2hhY21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMjU1MzQsImV4cCI6MjA5NDcwMTUzNH0.h5mIzDOvVS3M8BDFy3TeLM4djdBFHTM72LOpKGNgLkg
